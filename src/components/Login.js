a22bfef41565a7f923f99bde2f3cd44af5c34e19
Anudeep Debbata <30561588+Anudepp@users.noreply.github.com>
2025-07-23 16:31:14 +0530

    project Scaffold

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <img
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/a927b1ee-784d-494a-aa80-cf7a062d2523/web/IN-en-20250714-TRIFECTA-perspective_5acb7337-c372-45ec-ae12-ddb110e6ad78_large.jpg"
+        alt="Login"
+      />
+    </div>
+  );
+};
+
+export default Login;
